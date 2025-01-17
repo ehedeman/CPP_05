6647b80a70ae48b1408fb7a237c95d8031f1591f
Emma Hedemann <ehedeman@c2r2s6.42wolfsburg.de>
2025-01-17 15:35:01 +0100

    No more if else in Intern

--- ex03/Intern.cpp
+++ ex03/Intern.cpp
@@ -6,7 +6,7 @@
 /*   By: ehedeman <ehedeman@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/17 13:52:09 by ehedeman          #+#    #+#             */
-/*   Updated: 2025/01/17 14:11:36 by ehedeman         ###   ########.fr       */
+/*   Updated: 2025/01/17 15:34:22 by ehedeman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,17 +36,40 @@ Intern					&Intern::operator=(const Intern &copy)
 	else return (*this);
 }
 
+AForm	*Intern::makePres(std::string _target)
+{
+	AForm *form = new PresidentialPardonForm(_target);
+	return (form);
+}
+
+AForm	*Intern::makeShrub(std::string _target)
+{
+	AForm *form = new ShrubberyCreationForm(_target);
+	return (form);
+}
+
+AForm	*Intern::makeRobo(std::string _target)
+{
+	AForm *form = new RobotomyRequestForm(_target);
+	return (form);
+}
+
 AForm	*Intern::makeForm(std::string form_name, std::string target_name)
 {
 	AForm *form;
-	if (form_name == "ShrubberyCreationForm")
-		form = new ShrubberyCreationForm(target_name);
-	else if (form_name == "RobotomyRequestForm")
-		form = new RobotomyRequestForm(target_name);
-	else if (form_name == "PresidentialPardonForm")
-		form = new PresidentialPardonForm(target_name);
-	else
-		throw NoSuchFormException();
+
+	AForm *(Intern::*functionPointers[])(std::string) = \
+		{&Intern::makeShrub, &Intern::makeRobo, &Intern::makePres};
+	std::string forms[] = {"ShrubberyCreationForm", "RobotomyRequestForm", "PresidentialPardonForm"};
+	for (int i = 0; i < 3; i++)
+	{
+		if (form_name == forms[i])
+		{
+			form = (this->*functionPointers[i])(target_name);
+			return (form) ;
+		}
+	}
+	throw NoSuchFormException();
 	return (form);
 }
 
--- ex03/Intern.hpp
+++ ex03/Intern.hpp
@@ -6,7 +6,7 @@
 /*   By: ehedeman <ehedeman@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/17 13:52:05 by ehedeman          #+#    #+#             */
-/*   Updated: 2025/01/17 14:01:36 by ehedeman         ###   ########.fr       */
+/*   Updated: 2025/01/17 15:33:01 by ehedeman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,10 @@ public:
 	Intern					&operator=(const Intern &copy);
 
 	AForm					*makeForm(std::string form_name, std::string target_name);
+	AForm					*makeShrub(std::string _target);
+	AForm					*makePres(std::string _target);
+	AForm					*makeRobo(std::string _target);
+
 	class 					NoSuchFormException : public std::exception
 	{
 		public:
